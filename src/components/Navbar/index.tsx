2c64f4657815b02dbd0df2252b831f820a2a5de3
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-19 19:50:24 -0300

    Add github repo redirect  in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,6 @@
 import styles from './index.module.scss'
+import {FiGithub} from 'react-icons/fi'
+import {BsGithub} from 'react-icons/bs'
 
 import Link from 'next/link'
 
@@ -13,6 +15,13 @@ export default function Navbar(){
                     <Link href="/mylist"><button>Minha Lista</button></Link>
                 </li>
             </ul>
+            
+            <div className={styles.gitIconContainer}>
+                <a href='https://github.com/joaopedroalb/weather-challenge'>
+                    <BsGithub size={30}/>
+                </a>
+            </div>
+
         </nav>
     )
 }
